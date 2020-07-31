8d0cba81c8860bd786da31571662cc6ade76d7b7
unknown <ilian2005tttt@gmail.com>
2020-07-31 08:16:34 +0300

    Dobavqm filove

---
+++ TikTakToe.py
@@ -0,0 +1,48 @@
+from tkinter import *
+
+
+o_ta = StringVar()
+x_ove = StringVar()
+# napravi gi tez
+
+
+koi = 0
+
+x = [0, 0, 0]
+y = [0, 0, 0]
+d = [0, 0]
+
+x_o = [StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar()]
+for x in x_o:
+    x.set(".")
+
+def puton(c):
+    global x, y, d
+    global koi
+    global x_o
+    if koi % 2 == 0:
+        x[(c % 3) - 1] += 4
+        y[c // 3] += 4
+        if (c // 2) == 1 and 1 != c != 9:
+            d[1] += 4
+        if (c // 2) == 1 and 3 != c != 7:
+            d[2] += 4
+
+        #cikli za proverka dali ima popalneni redove
+
+    else:
+        x[(c % 3) - 1] += 1
+        y[c // 3] += 1
+        if (c // 2) == 1 and 1 != c != 9:
+            d[1] += 1
+        if (c // 2) == 1 and 3 != c != 7:
+            d[2] += 1
+
+
+
+echo "# Mrski-shah" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/IlVesAr/Mrski-shah.git
+git push -u origin master
